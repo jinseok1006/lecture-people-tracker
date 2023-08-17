bea2a3f86b9378758300585efd8d21140c9bfac9
jinseok1006 <jinseok1006@naver.com>
2023-08-17 17:32:07 +0900

    update

--- src/app.ts
+++ src/app.ts
@@ -11,6 +11,8 @@ const transporter = nodeMailer.createTransport({
   secure: false,
 });
 
+let latestMailingTime = new Date(0);
+
 export async function run(subject: string, div: string): Promise<void> {
   const payload = getPayload(subject);
 
@@ -18,9 +20,18 @@ export async function run(subject: string, div: string): Promise<void> {
     const data = await request(payload);
     const [numberOfPeople, seat] = parse(data, div);
 
+    // 인원 변동 확인
     if (numberOfPeople === seat) {
+      return console.log(`${subject} ${div}분반: ${numberOfPeople}/${seat} 최대인원`);
+    }
+
+    // 최근에 발송한 경우 미발송
+    const now = new Date();
+    const gap = now.getTime() - latestMailingTime.getTime();
+    const gapMinutes = (gap / 1000) * 60;
+    if (gapMinutes >= 5) {
       return console.log(
-        `${subject} ${div}분반: ${numberOfPeople}/${seat} 최대인원`
+        `마지막 메일 발송 시각: ${latestMailingTime.getHours()}:${latestMailingTime.getMinutes()}`
       );
     }
 
@@ -28,7 +39,8 @@ export async function run(subject: string, div: string): Promise<void> {
       to: process.env.MAIL_USER,
       subject: `${subject} ${div}분반 ${numberOfPeople}/${seat} 정원 감소`,
     });
-    console.log('메일발송', res);
+    latestMailingTime = now;
+    console.log('메일발송 성공', res);
   } catch (err) {
     console.log(err);
   }
--- src/bin.ts
+++ src/bin.ts
@@ -1,15 +1,25 @@
 import { run } from './app';
 const [node, bin, subject, div, interval] = process.argv;
+const intervalSecond = parseFloat(interval);
 
 if (process.env.NODE_ENV === 'production') {
   if (subject && interval && div) {
-    const trackingIntervalMinute = parseInt(interval);
-
-    console.log(`${subject} ${div}분반 ${interval}분 마다 요청...`);
-    setInterval(() => run(subject, div), 1000 * 60 * trackingIntervalMinute);
+    console.log(`${subject} ${div}분반 ${interval}초 마다 요청...`);
+    // setInterval(() => run(subject, div), 1000 * 60 * trackingIntervalMinute);
+    tracker(subject, div);
   } else {
     console.error('강의명 분반 트래킹간격 순으로 인자를 입력해주세요');
   }
 } else {
   setInterval(() => run('알고리즘', '4'), 5000);
+}
+
+function tracker(subject: string, div: string) {
+  const trackingInterval = 1000 * intervalSecond;
+  const mutation = (Math.random() * 5 - 2.5) * 1000;
+
+  setTimeout(() => {
+    run(subject, div);
+    tracker(subject, div);
+  }, trackingInterval + mutation);
 }
